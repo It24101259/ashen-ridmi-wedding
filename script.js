7028e44188f0c004b0400c8406d1603e81787a1e
Ashen Bandara <it24101259@my.sliit.lk>
2026-06-09 20:34:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -277,3 +277,18 @@ showGuestPopup();
 },1500);
 
 }
+const cards=document.querySelectorAll(".card");
+
+window.addEventListener("scroll",()=>{
+
+cards.forEach(card=>{
+
+const top=card.getBoundingClientRect().top;
+
+if(top<window.innerHeight-100){
+card.classList.add("show");
+}
+
+});
+
+});

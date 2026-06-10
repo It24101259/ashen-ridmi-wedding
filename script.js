dca14023acfc5f0292ac9792e6a8cc4757ddec7b
Ashen Bandara <it24101259@my.sliit.lk>
2026-06-10 22:31:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -428,3 +428,26 @@ document
 .src=this.src;
 
 }
+const cards =
+document.querySelectorAll(
+'.card,.royal-card'
+);
+
+window.addEventListener(
+'scroll',
+()=>{
+
+cards.forEach(card=>{
+
+const top =
+card.getBoundingClientRect().top;
+
+if(top < window.innerHeight-100){
+
+card.classList.add('show');
+
+}
+
+});
+
+});
